f038b1b237ecad1a31b7d43e668f3eeade90c66a
Matheus Silva <msilvarodrigues011@gmail.com>
2026-06-02 15:55:43 -0300

    Checkmate logic

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -20,8 +20,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while(true) {
-
+        while(!chessMatch.getCheckMate()) {
             try {
 
                 UI.clearScreen();
@@ -52,5 +51,7 @@ public class Program {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
